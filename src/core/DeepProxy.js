372ce77de14314fb1190a5cd5fdef94f5191efb1
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2021-04-14 18:21:05 +0800

    target

--- src/core/DeepProxy.js
+++ src/core/DeepProxy.js
@@ -1,11 +1,20 @@
 import Core from './Core.js'
 
+const deepProxyTargetSymbol = Symbol('deep.proxy.target');
+const deepProxyTargetOriginalSymbol = Symbol('deep.proxy.target.original');
+
 function create(obj, handler, preProscessor, keyFilter, parent, key) {
     key = key || '';
     const proxyHandler = {
+        set: (target, propKey, value, receiver) => {
+            if (propKey !== deepProxyTargetSymbol && propKey !== deepProxyTargetOriginalSymbol) {
+                Reflect.set(target, propKey, value, receiver);
+            }
+            return true;
+        },
         get: (target, propKey, receiver) => {
             let v = Reflect.get(target, propKey, receiver);
-            if (typeof v === 'object' && v !== null && (!keyFilter || keyFilter(propKey))) {
+            if (typeof v === 'object' && v !== null && propKey !== deepProxyTargetSymbol && propKey !== deepProxyTargetOriginalSymbol && (!keyFilter || keyFilter(propKey))) {
                 return create(v, handler, preProscessor, keyFilter, target, propKey);
             }
             return v;
@@ -16,8 +25,13 @@ function create(obj, handler, preProscessor, keyFilter, parent, key) {
         preProscessor(obj, parent, key);
     }
     let childProxy = new Proxy(obj, handler);
-
-    return new Proxy(childProxy, proxyHandler);
+    
+    if (!obj[deepProxyTargetOriginalSymbol]) {
+        obj[deepProxyTargetOriginalSymbol] = obj;
+    }
+    let proxy = new Proxy(childProxy, proxyHandler);
+    proxy[deepProxyTargetSymbol] = obj;
+    return proxy;
 }
 
 export default {
