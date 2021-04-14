48fb5cf0d0385f44ff274a343b57b2c0dee3814f
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2021-04-14 18:19:31 +0800

    class ShadowData

--- src/core/Core.js
+++ src/core/Core.js
@@ -1,5 +1,31 @@
+import ShadowData from './ShadowData.js';
+
 const shadowSymbol = Symbol("shadow");
 
+function getWatchers(obj, key) {
+    let watcherMapper = resolveWacherMapper(obj);
+    let watchers = watcherMapper[key] = watcherMapper[key] || new Set();
+    return watchers;
+}
+
+function resolveWacherMapper(obj) {
+    return resolveShadow(obj).watcherMapper;
+}
+
+function resolveShadow(target) {
+    if (target instanceof ShadowData) {
+        return target;
+    }
+    let shadow = target[shadowSymbol];
+    if (!shadow) {
+        shadow = new ShadowData();
+        target[shadowSymbol] = shadow;
+    }
+    return shadow;
+}
+
 export default {
-	shadowSymbol,
+    shadowSymbol,
+    getWatchers,
+    resolveShadow 
 }
---
+++ src/core/ShadowData.js
@@ -0,0 +1,7 @@
+class ShadowData {
+    constructor() {
+        this.watcherMapper = {};
+    }
+}
+
+export default ShadowData;
